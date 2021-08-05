530d66de6d4cf036ef89fc094250f3c9853031bb
AndresGiraldo7 <85905642+AndresGiraldo7@users.noreply.github.com>
2021-08-05 14:43:00 -0500

    Segunda actualizacion

--- PyhonReto1/Reto 1 Ciclo1.py
+++ PyhonReto1/Reto 1 Ciclo1.py
@@ -4,3 +4,6 @@ def abstracta ( lado : int , cubos : int = 4) -> tuple :
     return lado , area , volumen
 
 print(abstracta(37,9))
+print ( abstracta (41 , 2) )
+print ( abstracta (26 , 4) )
+print ( abstracta (17 , 5) )
